1418bb4faf726433c6b0934812f0d2f3a819032c
Talent Antonio <antonio.talenty@gmail.com>
2019-12-09 15:22:06 +0000

    new html inside slctor

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-console.log( "it works")
+// console.log( "it works")
 $(document).ready(function () {
-    
+    $('.submit').click(function (event) {
+        event.preventDefault()
+        console.log('clicked button')
+
+        var fullname = $('.fullname').val()
+        var email = $('.email').val()
+        var mobile = $('.mobile').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+       
+        if(email.length > 5 && email.includes && email.includes('.')){
+            statusElm.append( 'email is valid')
+        }else{
+            statusElm.append('Email is invalid')  
+        }
+    })
 })
